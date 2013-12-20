e241acd04fccae0b9dbe28611726612512ae2b18
videau <videau@b157b588-1f30-0410-9723-9032d8673973>
2013-12-20 16:44:55 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pastel/code-generator@356 b157b588-1f30-0410-9723-9032d8673973

--- SPECFEM3D/SPECFEM3D_compute_coupling_fluid_CMB_kernel.rb
+++ SPECFEM3D/SPECFEM3D_compute_coupling_fluid_CMB_kernel.rb
@@ -3,15 +3,15 @@ module BOAST
     push_env( :array_start => 0 )
     kernel = CKernel::new
     function_name = "compute_coupling_fluid_CMB_kernel"
-    displ_crust_mantle        = Real("displ_crust_mantle",        :dir => :in, :dim => [ Dim() ])
+    displ_crust_mantle        = Real("displ_crust_mantle",        :dir => :in,  :dim => [ Dim() ])
     accel_outer_core          = Real("accel_outer_core",          :dir => :out, :dim => [ Dim() ])
-    ibool_crust_mantle        = Int( "ibool_crust_mantle",        :dir => :in, :dim => [ Dim() ])
-    ibelm_bottom_crust_mantle = Int( "ibelm_bottom_crust_mantle", :dir => :in, :dim => [ Dim() ])
-    normal_top_outer_core     = Real("normal_top_outer_core",     :dir => :in, :dim => [ Dim() ])
-    jacobian2D_top_outer_core = Real("jacobian2D_top_outer_core", :dir => :in, :dim => [ Dim() ])
-    wgllwgll_xy               = Real("wgllwgll_xy",               :dir => :in, :dim => [ Dim() ])
-    ibool_outer_core          = Int( "ibool_outer_core",          :dir => :in, :dim => [ Dim() ])
-    ibelm_top_outer_core      = Int( "ibelm_top_outer_core",      :dir => :in, :dim => [ Dim() ])
+    ibool_crust_mantle        = Int( "ibool_crust_mantle",        :dir => :in,  :dim => [ Dim() ])
+    ibelm_bottom_crust_mantle = Int( "ibelm_bottom_crust_mantle", :dir => :in,  :dim => [ Dim() ])
+    normal_top_outer_core     = Real("normal_top_outer_core",     :dir => :in,  :dim => [ Dim() ])
+    jacobian2D_top_outer_core = Real("jacobian2D_top_outer_core", :dir => :in,  :dim => [ Dim() ])
+    wgllwgll_xy               = Real("wgllwgll_xy",               :dir => :in,  :dim => [ Dim() ])
+    ibool_outer_core          = Int( "ibool_outer_core",          :dir => :in,  :dim => [ Dim() ])
+    ibelm_top_outer_core      = Int( "ibelm_top_outer_core",      :dir => :in,  :dim => [ Dim() ])
     nspec2D_TOP_OC            = Int( "NSPEC2D_TOP_OC",            :dir => :in)
 
     ndim =               Int( "NDIM",              :const => 3)
@@ -31,23 +31,17 @@ module BOAST
       load "./INDEX3.rb"
       load "./INDEX4.rb"
       decl p
-      decl i =          Int("i")
-      decl j =          Int("j")
-      decl k =          Int("k")
+      decl i = Int("i"), j = Int("j"), k = Int("k")
       decl iface =      Int("iface")
       decl k_corresp =  Int("k_corresp")
       decl iglob_cm =   Int("iglob_cm")
       decl iglob_oc =   Int("iglob_oc")
       decl ispec =      Int("ispec")
       decl ispec_selected = Int("ispec_selected")
-      decl displ_x =    Real("displ_x")
-      decl displ_y =    Real("displ_y")
-      decl displ_z =    Real("displ_z")
-      decl displ_n =    Real("displ_n")
-      decl nx      =    Real("nx")
-      decl ny      =    Real("ny")
-      decl nz      =    Real("nz")
-      decl weight  =    Real("weight")
+      decl *(displ = [Real("displ_x"), Real("displ_y"), Real("displ_z")])
+      decl displ_n = Real("displ_n")
+      decl *(n = [Real("nx"), Real("ny"), Real("nz")])
+      decl weight = Real("weight")
 
       print i === get_local_id(0)
       print j === get_local_id(1)
@@ -58,13 +52,9 @@ module BOAST
         print k === ngllx - 1
         print k_corresp === 0
         print iglob_cm === ibool_crust_mantle[INDEX4(ngllx,ngllx,ngllx,i,j,k_corresp,ispec_selected)] - 1
-        print displ_x === displ_crust_mantle[iglob_cm*3]
-        print displ_y === displ_crust_mantle[iglob_cm*3+1]
-        print displ_z === displ_crust_mantle[iglob_cm*3+2]
-        print nx === normal_top_outer_core[INDEX4(ndim,ngllx,ngllx,0,i,j,iface)]
-        print ny === normal_top_outer_core[INDEX4(ndim,ngllx,ngllx,1,i,j,iface)]
-        print nz === normal_top_outer_core[INDEX4(ndim,ngllx,ngllx,2,i,j,iface)]
-        print displ_n === displ_x*nx + displ_y*ny + displ_z*nz
+        displ.each_index { |indx| print displ[indx] === displ_crust_mantle[iglob_cm*3+indx] }
+        n.each_index { |indx| print n[indx] === normal_top_outer_core[INDEX4(ndim,ngllx,ngllx,indx,i,j,iface)] }
+        print displ_n === displ[0]*n[0] + displ[1]*n[1] + displ[2]*n[2]
         print weight === jacobian2D_top_outer_core[INDEX3(ngllx,ngllx,i,j,iface)]*wgllwgll_xy[INDEX2(ngllx,i,j)]
         print iglob_oc === ibool_outer_core[INDEX4(ngllx,ngllx,ngllx,i,j,k,ispec)] - 1
         print atomicAdd(accel_outer_core+iglob_oc, weight*displ_n)
