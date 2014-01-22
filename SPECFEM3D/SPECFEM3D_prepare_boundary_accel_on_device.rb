34169c97e939802b51950f0a38538c126c585b9d
videau <videau@b157b588-1f30-0410-9723-9032d8673973>
2014-01-22 13:49:18 +0000

    Refactoring
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/pastel/code-generator@371 b157b588-1f30-0410-9723-9032d8673973

--- SPECFEM3D/SPECFEM3D_prepare_boundary_accel_on_device.rb
+++ SPECFEM3D/SPECFEM3D_prepare_boundary_accel_on_device.rb
@@ -1,14 +1,30 @@
 module BOAST
   def BOAST::prepare_boundary_accel_on_device(ref = true)
+    BOAST::prepare_boundary_on_device(:accel, ref)
+  end
+  def BOAST::prepare_boundary_on_device(type, ref = true)
     push_env( :array_start => 0 )
     kernel = CKernel::new
-    function_name = "prepare_boundary_accel_on_device"
+
     num_interfaces =        Int( "num_interfaces",        :dir => :in)
     max_nibool_interfaces = Int( "max_nibool_interfaces", :dir => :in)
-    d_accel =               Real("d_accel",               :dir => :in,  :dim => [ Dim() ] )
-    d_send_accel_buffer =   Real("d_send_accel_buffer",   :dir => :out, :dim => [ Dim(num_interfaces*max_nibool_interfaces*3) ] )
+    if type == :accel then
+      function_name = "prepare_boundary_accel_on_device"
+      d_accel =             Real("d_accel",             :dir => :in,  :dim => [ Dim() ] )
+      d_send_accel_buffer = Real("d_send_accel_buffer", :dir => :out, :dim => [ Dim(num_interfaces*max_nibool_interfaces*3) ] )
+      variables = [ d_accel, d_send_accel_buffer ]
+    elsif type == :potential then
+      function_name = "prepare_boundary_potential_on_device"
+      d_potential_dot_dot_acoustic =    Real("d_potential_dot_dot_acoustic",    :dir => :in, :dim => [ Dim() ] )
+      d_send_potential_dot_dot_buffer = Real("d_send_potential_dot_dot_buffer", :dir => :out,:dim => [ Dim(num_interfaces*max_nibool_interfaces) ] )
+      variables = [ d_potential_dot_dot_acoustic, d_send_potential_dot_dot_buffer ]
+    else
+      raise "Unsupported boundary type: #{type}!"
+    end
     d_nibool_interfaces =   Int( "d_nibool_interfaces",   :dir => :in,  :dim => [ Dim(num_interfaces) ] )
     d_ibool_interfaces =    Int( "d_ibool_interfaces",    :dir => :in,  :dim => [ Dim(num_interfaces*max_nibool_interfaces) ] )
+
+    variables += [ num_interfaces, max_nibool_interfaces, d_nibool_interfaces, d_ibool_interfaces]
     p = Procedure::new(function_name, [d_accel,d_send_accel_buffer,num_interfaces,max_nibool_interfaces,d_nibool_interfaces,d_ibool_interfaces])
     if(get_lang == CUDA and ref) then
       @@output.print File::read("specfem3D/#{function_name}.cu")
@@ -30,9 +46,13 @@ module BOAST
         print If(id<d_nibool_interfaces[iinterface]) {
           print iloc === id + max_nibool_interfaces*iinterface
           print iglob === d_ibool_interfaces[iloc] - 1
-          (0..2).each { |i|
-            print d_send_accel_buffer[iloc*3 + i] === d_accel[iglob*3 + i]
-          }
+          if type == :accel then
+            (0..2).each { |i|
+              print d_send_accel_buffer[iloc*3 + i] === d_accel[iglob*3 + i]
+            }
+          else
+            print d_send_potential_dot_dot_buffer[iloc] === d_potential_dot_dot_acoustic[iglob]
+          end
         }
       }
       close p
--- SPECFEM3D/SPECFEM3D_prepare_boundary_potential_on_device.rb
+++ SPECFEM3D/SPECFEM3D_prepare_boundary_potential_on_device.rb
@@ -1,44 +1,7 @@
+require './SPECFEM3D_prepare_boundary_accel_on_device.rb'
+
 module BOAST
   def BOAST::prepare_boundary_potential_on_device(ref = true)
-    push_env( :array_start => 0 )
-    kernel = CKernel::new
-    function_name = "prepare_boundary_potential_on_device"
-    num_interfaces =                  Int( "num_interfaces",                  :dir => :in)
-    max_nibool_interfaces =           Int( "max_nibool_interfaces",           :dir => :in)
-    d_potential_dot_dot_acoustic =    Real("d_potential_dot_dot_acoustic",    :dir => :in, :dim => [ Dim() ] )
-    d_send_potential_dot_dot_buffer = Real("d_send_potential_dot_dot_buffer", :dir => :out,:dim => [ Dim(num_interfaces*max_nibool_interfaces) ] )
-    d_nibool_interfaces =             Int( "d_nibool_interfaces",             :dir => :in, :dim => [ Dim(num_interfaces) ] )
-    d_ibool_interfaces =              Int( "d_ibool_interfaces",              :dir => :in, :dim => [ Dim(num_interfaces*max_nibool_interfaces) ] )
-    p = Procedure(function_name, [d_potential_dot_dot_acoustic,d_send_potential_dot_dot_buffer,num_interfaces,max_nibool_interfaces,d_nibool_interfaces,d_ibool_interfaces])
-    if(get_lang == CUDA and ref) then
-      @@output.print File::read("specfem3D/#{function_name}.cu")
-    elsif(get_lang == CUDA or get_lang == CL) then
-      if(get_lang == CL and get_default_real_size == 8) then
-        @@output.puts "#pragma OPENCL EXTENSION cl_khr_fp64: enable"
-      end
-      decl p
-      id = Int("id")
-      iglob = Int("iglob")
-      iloc = Int("iloc")
-      iinterface = Int("iinterface")
-      decl id
-      decl iglob
-      decl iloc
-      decl iinterface
-      print id === get_global_id(0)+get_global_size(0)*get_global_id(1)
-      print For(iinterface, 0, num_interfaces-1) {
-        print If(id<d_nibool_interfaces[iinterface]) {
-          print iloc === id + max_nibool_interfaces*iinterface
-          print iglob === d_ibool_interfaces[iloc] - 1
-          print d_send_potential_dot_dot_buffer[iloc] === d_potential_dot_dot_acoustic[iglob]
-        }
-      }
-      close p
-    else
-      raise "Unsupported language!"
-    end
-    pop_env( :array_start )
-    kernel.procedure = p
-    return kernel
+    BOAST::prepare_boundary_on_device(:potential, ref)
   end
 end
