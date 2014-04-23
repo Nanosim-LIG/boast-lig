2497d819af4c7bcd348f7b603ab70fbdd519e4fc
Brice Videau <brice.videau@imag.fr>
2014-04-23 11:10:55 +0200

    validated prepare_boundary_accel_on_device.cu prepare_boundary_potential_on_device.cu

--- SPECFEM3D/references/prepare_boundary_potential_on_device.cu
+++ SPECFEM3D/references/prepare_boundary_potential_on_device.cu
@@ -1,5 +1,12 @@
 // from assemble_MPI_scalar_cuda.cu
-__global__ void prepare_boundary_potential_on_device(realw* d_potential_dot_dot_acoustic,realw* d_send_potential_dot_dot_buffer,int num_interfaces,int max_nibool_interfaces,int* d_nibool_interfaces,int* d_ibool_interfaces) {
+typedef float realw;
+
+__global__ void prepare_boundary_potential_on_device(realw* d_potential_dot_dot_acoustic,
+                                                     realw* d_send_potential_dot_dot_buffer,
+                                                     int num_interfaces,
+                                                     int max_nibool_interfaces,
+                                                     int* d_nibool_interfaces,
+                                                     int* d_ibool_interfaces) {
 
   int id = threadIdx.x + blockIdx.x*blockDim.x + blockIdx.y*gridDim.x*blockDim.x;
   int iglob,iloc;
