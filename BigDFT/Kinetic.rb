6412403b52974719f25ca87eff77f18b64374bcd
Brice Videau <brice.videau@imag.fr>
2014-06-06 17:27:27 +0200

    mod_arr cannot be deactivated for Kinetic.

--- BigDFT/Kinetic.rb
+++ BigDFT/Kinetic.rb
@@ -1068,7 +1068,7 @@ EOF
     return kernel
   end
 
-  def BOAST::kinetic(filt, center, unroll, ekin = false, free=[0,0,0],mod_arr=[false,false,false])
+  def BOAST::kinetic(filt, center, unroll, ekin = false, free=[0,0,0])
     kernel = CKernel::new
     BOAST::set_output( kernel.code )
     kernel.lang = BOAST::get_lang
--- BigDFT/Kinetic_test.rb
+++ BigDFT/Kinetic_test.rb
@@ -66,7 +66,7 @@ diff.each { |elem|
 }
 output = NArray.float(n1,n2,n3).random
 (0..3).each{ |unroll|
-  k = BOAST::kinetic(filt,14,[3,1,12],false,[0]*3,[true]*3)
+  k = BOAST::kinetic(filt,14,[3,1,12],false,[0]*3)
   #k.build({:FC => 'gfortran',:CC => 'gcc',:FCFLAGS => "-O2 -fbounds-check",:LDFLAGS => "-lgfortran"})
   #k.print
   #k.build({:FC => 'gfortran',:CC => 'gcc',:FCFLAGS => "-O2 -fopenmp",:LDFLAGS => "-fopenmp"})
