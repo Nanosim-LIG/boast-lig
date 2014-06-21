66d9bed10b9ec63eb9634b6333b1112c5487a864
Brice Videau <brice.videau@imag.fr>
2014-06-21 14:12:38 +0200

    added test for invert periodic magicfilter operation

--- BigDFT/MagicFilter_test2.rb
+++ BigDFT/MagicFilter_test2.rb
@@ -25,7 +25,7 @@ pad_in = 0
 pad_out = 2
 input = NArray.float(n1+pad_in,n2+pad_in,n3+pad_in,m).random
 (1..(m-1)).each { |i|
-  input[0..(n1+pad_in-1), 0..(n2+pad_in-1), 0..(n3+pad_in-1), i] = input[0..(n1+pad_in-1), 0..(n2+pad_in-1), 0..(n3+pad_in-1), 0]
+  input[ true, true, true, i] = input[ true, true, true, 0]
 }
 work1 = NArray.float(n1+pad_out,n2+pad_out,n3+pad_out,m)
 work2 = NArray.float(n1+pad_out,n2+pad_out,n3+pad_out,m)
@@ -93,7 +93,7 @@ work2 = NArray.float(n1+15,n2+15,n3+15,m)
 output_ref = NArray.float(n1+15,n2+15,n3+15)
 output = NArray.float(n1+15,n2+15,n3+15,m)
 (1..(m-1)).each { |i|
-  input[0..(n1-1), 0..(n2-1), 0..(n3-1),i] = input[0..(n1-1), 0..(n2-1), 0..(n3-1),0]
+  input[ true, true, true, i ] = input[ true, true, true, 0 ]
 }
 
 k_ref = BOAST::magicfilter_per_ref(false,true)
@@ -138,7 +138,7 @@ work2 = NArray.float(n1+15,n2+15,n3+15,m)
 output_ref = NArray.float(n1,n2,n3)
 output = NArray.float(n1,n2,n3,m)
 (1..(m-1)).each { |i|
-  input[0..(n1+15-1), 0..(n2+15-1), 0..(n3+15-1), i] = input[0..(n1+15-1), 0..(n2+15-1), 0..(n3+15-1), 0]
+  input[ true, true, true, i ] = input[ true, true, true, 0 ]
 }
 
 k_ref = BOAST::magicfilter_per_ref(true,true)
@@ -181,4 +181,64 @@ m.times { |i|
   }
 }
 puts "#{k.procedure.name}: #{stats[:duration]*1.0e3} #{k.cost(n, bc, m) / (stats[:duration]*1.0e9)} GFlops"
+
+puts 'again, periodic inverse'
+input = NArray.float(n1+pad_in,n2+pad_in,n3+pad_in,m).random
+(1..(m-1)).each { |i|
+  input[ true, true, true, i] = input[ true, true, true, 0]
+}
+work1 = NArray.float(n1+pad_out,n2+pad_out,n3+pad_out,m)
+work2 = NArray.float(n1+pad_out,n2+pad_out,n3+pad_out,m)
+output_ref = NArray.float(n1,n2,n3)
+output = NArray.float(n1+pad_out,n2+pad_out,n3+pad_out,m)
+epsilon = 10e-15
+
+n = NArray.int(3)
+n[0] = n1
+n[1] = n2
+n[2] = n3
+
+ld_in = NArray.int(3)
+ld_in[0] = n1+pad_in
+ld_in[1] = n2+pad_in
+ld_in[2] = n3+pad_in
+
+ld_out = NArray.int(3)
+ld_out[0] = n1+pad_out
+ld_out[1] = n2+pad_out
+ld_out[2] = n3+pad_out
+
+bc = NArray.int(3)
+
+k_ref = BOAST::magicfilter_per_ref( true )
+stats = k_ref.run(n1, n2*n3, input, output_ref)
+stats = k_ref.run(n2, n1*n3, output_ref, work1)
+stats = k_ref.run(n3, n2*n1, work1, output_ref)
+
+puts "#{k_ref.procedure.name}: #{stats[:duration]*1.0e3} #{32*n1*n2*n3 / (stats[:duration]*1.0e9)} GFlops"
+
+bc[0] = BOAST::BC::PERIODIC
+bc[1] = BOAST::BC::PERIODIC
+bc[2] = BOAST::BC::PERIODIC
+
+repeat = 5
+begin
+  stats_a = []
+  repeat.times { 
+    stats_a.push k.run(3, n, bc, ld_in, ld_out, m, input, output, work1, work2)
+  }
+rescue Exception => e
+  puts e.inspect
+end
+stats_a.sort_by! { |a| a[:duration] }
+stats = stats_a.first
+
+m.times { |i|
+  diff = (output_ref - output[0..(n1-1), 0..(n2-1), 0..(n3-1), i]).abs
+  diff.each { |elem|
+    raise "Warning: residue too big: #{elem}" if elem > epsilon
+  }
+}
+puts "#{k.procedure.name}: #{stats[:duration]*1.0e3} #{k.cost(n, bc, m) / (stats[:duration]*1.0e9)} GFlops"
+
 
