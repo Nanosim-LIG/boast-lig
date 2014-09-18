0f2fed1387151677ab68bb4c599b01408633880d
Brice Videau <brice.videau@imag.fr>
2014-09-19 00:08:24 +0200

    Chri example redux.

--- ARMclbench/Laplacian.rb
+++ ARMclbench/Laplacian.rb
@@ -1,62 +1,184 @@
 require 'BOAST'
 include BOAST
+require 'narray'
+
+class Numeric
+  def clamp( min, max )
+    [[self, max].min, min].max
+  end
+end
+
+def rndup( val, div)
+  return (val%div) == 0 ? val : val + div - (val%div)
+end
 
 set_lang(CL)
-
-register_funccall("MAX")
-register_funccall("MIN")
-
 set_array_start(0)
+register_funccall("clamp")
 
-height = Int("height")
-width = Int("width")
-pdst = Int("pdst", :dir => :in,  :signed => false, :size => 1, :dim => [ Dim(3), Dim(width), Dim(height) ] )
-psrc = Int("psrc", :dir => :out, :signed => false, :size => 1, :dim => [ Dim(3), Dim(width), Dim(height) ] )
+def laplacian_ref( input, output, width, height )
+  (1...(height-1)).each { |y_i|
+    (3...(3*width-3)).each { |x_i|
+      output[x_i, y_i] = ( -input[x_i - 3, y_i - 1] - input[x_i, y_i - 1] - input[x_i + 3, y_i - 1]\
+                          - input[x_i - 3, y_i]     + input[x_i, y_i] * 9 - input[x_i + 3, y_i]\
+                          - input[x_i - 3, y_i + 1] - input[x_i, y_i + 1] - input[x_i + 3, y_i + 1] ).clamp(0,255)
+    }
+  }
+end
 
-get_output.puts <<EOF
-#define MAX(a,b) ((a)>(b)?(a):(b))
-#define MIN(a,b) ((a)<(b)?(a):(b))
-EOF
-p = Procedure("math", [pdst, psrc, width, height]) {
-  decl y = Int("y")
-  decl x = Int("x")
-  decl xBoundary = Int("xBoundary")
-  decl yBoundary = Int("yBoundary")
-  pr xBoundary === width - 2
-  pr yBoundary === height - 2
+def laplacian(x_component_number = 1, vector_length=1, y_component_number = 1, temporary_size = 4)
+ 
+  k = CKernel::new
+  height = Int("height", :dir => :in)
+  width = Int("width", :dir => :in)
+  w = Int("w")
+  pdst = Int("pdst", :dir => :out,  :signed => false, :size => 1, :dim => [ Dim(w), Dim(height) ] )
+  psrc = Int("psrc", :dir => :in, :signed => false, :size => 1, :dim => [ Dim(w), Dim(height) ] )
   
-  pr y === get_global_id(0)
-  pr x === get_global_id(1)
+  get_output.puts <<EOF
+#define vload1(offset, p) *(p + offset)
+#define vstore1(d, offset, p) *(p + offset) = d
+EOF
+  p = Procedure("math", [psrc, pdst, width, height]) {
+    decl y = Int("y")
+    decl x = Int("x")
+    decl w
+    
+    pr y === get_global_id(1)
+    pr x === get_global_id(0) * x_component_number 
+    pr w === width * 3
 
-  pr If( Expression( "||", x >= xBoundary, y >= yBoundary ), lambda {
-    (0..2).each { |ind|
-      pr pdst[ind, x, y] === psrc[ind, x, y]
-    }
-  }, lambda {
-    acolors = ["b", "g", "r"].collect { |c|
-      Int(c+"Color")
-    }
-    decl *acolors
-    acolors.each { |c|
-      pr c === 0
-    }
-    (0..2).each { |ind|
-      pr acolors[ind] === acolors[ind] - psrc[ind, x, y]     - psrc[ind, x + 1, y]         - psrc[ind, x + 2, y] \
-                                       - psrc[ind, x, y + 1] + psrc[ind, x + 1, y + 1] * 9 - psrc[ind, x + 2, y + 1] \
-                                       - psrc[ind, x, y + 2] - psrc[ind, x + 1, y + 2]     - psrc[ind, x + 2, y + 2]
-    }
-    colors = ["blue", "green", "red"].collect { |c|
-      Int(c, :size => 1, :signed => false)
-    }
-    decl *colors
-    (0..2).each { |ind|
-      pr colors[ind] === MAX(MIN(acolors[ind], 255), 0)
-    }
-    (0..2).each { |ind|
-      pr pdst[ind, x, y] === colors[ind]
-    }
-  })
+    pr x === Ternary(x < 3, 3, Ternary( x > w      - x_component_number - 3, w      - x_component_number - 3, x ) )
+    pr y === Ternary(y < 1, 1, Ternary( y > height - y_component_number - 1, height - y_component_number - 1, y ) )
 
+    vector_number = (x_component_number.to_f/vector_length).ceil
+    vector_number = vector_number < 1 ?  1 : vector_number
+    total_x_size = vector_number * vector_length
+
+    temp_type = "#{Int("dummy", :size => temporary_size).type.decl}"
+    
+
+    tempnn = (0..2).collect { |v_i|
+      (0...vector_number).collect { |x_i|
+        (0...(y_component_number+2)).collect { |y_i|
+          Int("temp#{x_i}#{v_i}#{y_i}", :size => 1, :vector_length => vector_length, :signed => false)
+        }
+      }
+    }
+    decl *(tempnn.flatten)
+    resnn = (0...(vector_number)).collect { |v_i|
+      (0...(y_component_number)).collect { |y_i|
+        Int("res#{v_i}#{y_i}", :size => 1, :vector_length => vector_length, :signed => false)
+      }
+    }
+    decl *(resnn.flatten)
+
+    tempcnn = (0..2).collect { |v_i|
+      (0...vector_number).collect { |x_i|
+        (0...(y_component_number+2)).collect { |y_i|
+          Int("tempc#{x_i}#{v_i}#{y_i}", :size => temporary_size, :vector_length => vector_length)
+        }
+      }
+    }
+    decl *(tempcnn.flatten)
+    rescnn = (0...vector_number).collect { |v_i|
+      (0...y_component_number).collect { |y_i|
+        Int("resc#{v_i}#{y_i}", :size => temporary_size, :vector_length => vector_length)
+      }
+    }
+    decl *(rescnn.flatten)
+
+    (0..2).each { |x_i|
+      (0...(y_component_number+2)).each { |y_i|
+        (0...vector_number).each { |v_i|
+          pr tempnn[x_i][v_i][y_i] === FuncCall("vload#{vector_length}",0, psrc[x + v_i * vector_length + 3 * (x_i - 1), y + (y_i - 1)].address)
+        }
+      }
+    }
+    (0..2).each { |x_i|
+      (0...vector_number).each { |v_i|
+        (0...(y_component_number+2)).each { |y_i|
+          pr tempcnn[x_i][v_i][y_i] === FuncCall("convert_#{tempcnn[0][0][0].type.decl}", tempnn[x_i][v_i][y_i])
+        }
+      }
+    }
+    (0...vector_number).each { |v_i|
+      (0...y_component_number).each { |y_i|
+        pr rescnn[v_i][y_i] === - tempcnn[0][v_i][y_i]     - tempcnn[1][v_i][y_i]         - tempcnn[2][v_i][y_i]\
+                                - tempcnn[0][v_i][y_i + 1] + tempcnn[1][v_i][y_i + 1] * "(#{temp_type})9" - tempcnn[2][v_i][y_i + 1]\
+                                - tempcnn[0][v_i][y_i + 2] - tempcnn[1][v_i][y_i + 2]     - tempcnn[2][v_i][y_i + 2]
+        pr resnn[v_i][y_i] === FuncCall("convert_#{tempnn[0][0][0].type.decl}", clamp(rescnn[v_i][y_i],"(#{temp_type})0","(#{temp_type})255"))
+      }
+    }
+
+    (0...(y_component_number)).each { |y_i|
+      remaining_elem = x_component_number
+      (0...vector_number).each { |v_i|
+        if remaining_elem >= vector_length then
+          pr FuncCall("vstore#{vector_length}", resnn[v_i][y_i], 0, pdst[x + v_i * vector_length, y + y_i].address)
+          remaining_elem -= vector_length
+        else
+          temp_vec_length = vector_length
+          begin
+            temp_vec_length = temp_vec_length/2
+            elem_indexes = 0
+            if remaining_elem >= temp_vec_length then
+              pr FuncCall("vstore#{temp_vec_length}", resnn[v_i][y_i].components(elem_indexes...(elem_indexes+temp_vec_length)), 0, pdst[x + (v_i * vector_length + elem_indexes), y + y_i].address)
+              elem_indexes += temp_vec_length
+              remaining_elem -= temp_vec_length
+            end
+          end while remaining_elem > 0
+        end
+      }
+    }
+  }
+  pr p
+  k.procedure = p
+  return k
+end
+#puts laplacian
+#puts laplacian(2)
+#puts laplacian(2,2)
+#puts laplacian(4,2)
+#puts laplacian(3,2)
+
+width = 1024
+height = 512
+
+input = NArray.byte(width*3,height).random(256)
+output_ref = NArray.byte(width*3,height)
+output = NArray.byte(width*3,height)
+
+laplacian_ref(input, output_ref, width, height)
+
+results = []
+
+(1..16).each{ |x_component_number|
+  [1,2,4,8,16].reject{ |v| v > x_component_number }.each{ |vector_length| # = 1, vector_length=1, y_component_number = 1, temporary_size = 4
+    (1..4).each { |y_component_number|
+      [2,4].each{ |temporary_size|
+        puts "x_component_number: #{x_component_number}, vector_length: #{vector_length}, y_component_number: #{y_component_number}, temporary_size: #{temporary_size}"
+        k = laplacian(x_component_number, vector_length, y_component_number, temporary_size)
+        output.random(256)
+        durations=[]
+        (0..3).each {
+          stats = k.run(input, output, width, height, :global_work_size => [rndup((width*3/x_component_number.to_f).ceil,32), height, 1], :local_work_size => [32, 1, 1])
+          durations.push stats[:duration]
+        }
+        puts durations.min
+      
+        diff = ( output_ref[3..-4,1..-2] - output[3..-4,1..-2] ).abs
+        i = 0
+        diff.each { |elem|
+          #puts elem
+          i += 1
+          raise "Warning: residue too big: #{elem} #{i%3},#{(i / 3 ) % (width-2)},#{i / 3 / (width - 2)}" if elem > 0
+        }
+        results.push( ["x_component_number: #{x_component_number}, vector_length: #{vector_length}, temporary_size #{temporary_size}", durations.min] )
+      }
+    }
+  }
 }
 
-pr p
+results.sort! { |x,y| x[1] <=> y[1] }
+puts results[0]
