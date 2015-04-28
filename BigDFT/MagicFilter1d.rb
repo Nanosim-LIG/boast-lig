6bfdbd6d64c278e06e5386912810e59a94d89c0e
Brice Videau <brice.videau@imag.fr>
2015-04-28 13:23:09 +0200

    Added 1d MagicFilter.

---
+++ BigDFT/MagicFilter1d.rb
@@ -0,0 +1,50 @@
+require './GenericConvolution.rb'
+require './WaveletFilters.rb'
+def MagicFilter1d(filter, optims=GenericOptimization::new)
+  conv_operation = GenericConvolutionOperator1d::new(filter, :ld => true)
+  conv_operation.optimize(optims) if optims
+
+  p, subops = conv_operation.procedure
+
+  kernel = BOAST::CKernel::new
+
+  print_header
+
+  subops.each_value { |op|
+    BOAST::pr op
+    puts "chosen:"+ op.name
+  }
+  BOAST::pr p
+
+  kernel.procedure = p
+  kernel.cost_function = lambda { |*args| conv_operation.cost(*args) }
+  return kernel
+end
+
+optims = GenericOptimization::new(:unroll_range => 6, :mod_arr_test => true, :tt_arr_test => true)
+conv_filter = ConvolutionFilter::new('sfrf',SYM8_MF,8)
+ksym8 = MagicFilter1d( conv_filter, optims )
+puts ksym8
+
+n1 = 128
+n2 = 128
+n3 = 128
+input = NArray.float(n1,n2,n3).random!
+work1 = NArray.float(n1,n2,n3)
+work2 = NArray.float(n1,n2,n3)
+output = NArray.float(n1,n2,n3)
+
+n = NArray.int(3)
+n[0] = n1
+n[1] = n2
+n[2] = n3
+
+ksym8.build(:openmp => true)
+stats0 = ksym8.run(3, 0, n, BC::PERIODIC, n, n, input, work1)
+stats0 = ksym8.run(3, 0, n, BC::PERIODIC, n, n, input, work1)
+stats1 = ksym8.run(3, 1, n, BC::PERIODIC, n, n, work1, work2)
+stats2 = ksym8.run(3, 2, n, BC::PERIODIC, n, n, work2, output)
+puts "#{ksym8.procedure.name} d0: #{stats0[:duration]*1.0e3} ms #{32*n1*n2*n3 / (stats0[:duration]*1.0e9)} GFlops"
+puts "#{ksym8.procedure.name} d1: #{stats1[:duration]*1.0e3} ms #{32*n1*n2*n3 / (stats1[:duration]*1.0e9)} GFlops"
+puts "#{ksym8.procedure.name} d2: #{stats2[:duration]*1.0e3} ms #{32*n1*n2*n3 / (stats2[:duration]*1.0e9)} GFlops"
+puts "#{ksym8.procedure.name}: #{(stats0[:duration]+stats1[:duration]+stats2[:duration])*1.0e3} ms #{3*32*n1*n2*n3 / ((stats0[:duration]+stats1[:duration]+stats2[:duration])*1.0e9)} GFlops"
