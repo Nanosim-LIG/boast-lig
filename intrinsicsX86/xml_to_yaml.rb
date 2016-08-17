74028a980d7bcc58558386f2f7cc096a1bdc201e
Brice Videau <brice.videau@imag.fr>
2016-08-17 18:26:38 +0200

    Updated to v3.3.14.

--- intrinsicsX86/xml_to_yaml.rb
+++ intrinsicsX86/xml_to_yaml.rb
@@ -1,13 +1,16 @@
 require 'yaml'
 require 'active_support/core_ext/hash/conversions'
-f = File::open('data-3.3.12.xml')
+f = File::open('data-3.3.14.xml')
 hash = Hash.from_xml(f.read)
 hash["intrinsics_list"]["intrinsic"].each { |intrinsic|
   list = []
   if intrinsic["CPUID"] then
-    [intrinsic["CPUID"]].flatten.each { |cpuid|
-      list += cpuid.split("/")
-    }
+    cpuid_list = [intrinsic["CPUID"]].flatten
+    if cpuid_list.length > 1 then
+      list += [cpuid_list]
+    elsif cpuid_list.length == 1 then
+      list += cpuid_list.first.split("/")
+    end
     intrinsic["CPUID"]=list
   end
   list = []
